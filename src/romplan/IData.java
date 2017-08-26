81f12ea30d6a40fc34640c131621d6b89e4122ce
Patrick Pedersen <patrick@ossia.net>
2017-08-26 13:54:37 +0200

    Added interface, class and methods for data read/write

---
+++ src/romplan/IData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romplan;
+
+import java.io.File;
+
+/**
+ *
+ * @author patrick
+ */
+public class IData implements Data {
+    
+    @Override
+    public void hente() {
+    }
+
+    @Override
+    public void lagre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
