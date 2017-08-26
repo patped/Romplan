440985a7548445f982ed4a70022f57551f2ba70f
Patrick Pedersen <patrick@ossia.net>
2017-08-26 19:17:23 +0200

    Deleted IData class and changed Data Interface to be a class

--- src/romplan/Data.java
+++ src/romplan/Data.java
@@ -5,11 +5,49 @@
  */
 package romplan;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author patrick
  */
-public interface Data {
-    public Reservasjon[] hente();
-    public void lagre();
+public class Data {
+    private static final String FILNAVN = "data.txt";
+    
+    public static Reservasjon[] hent() {
+        try {
+            String[] data;
+            int antall = 0;
+            Scanner leser;
+            Reservasjon[] resTab;
+            File fil = new File(FILNAVN);
+            leser = new Scanner(fil);
+            antall = Integer.parseInt(leser.nextLine());
+            resTab = new Reservasjon[antall-1];
+            int i = 0;
+            do {
+                data = leser.nextLine().split(";");
+                Dato dato = new Dato(Integer.parseInt(data[2]),Integer.parseInt(data[3]),Integer.parseInt(data[4]));
+                KlSlett fra = new KlSlett(Integer.parseInt(data[5]),Integer.parseInt(data[6]));
+                KlSlett til = new KlSlett(Integer.parseInt(data[7]),Integer.parseInt(data[8]));
+                resTab[i] = new Reservasjon(data[0],Integer.parseInt(data[1]),dato,fra,til);
+                i++;
+            } while (leser.hasNextLine());
+            leser.close();
+            return resTab;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Reservasjon[] fileNotFound = new Reservasjon[0];
+        return fileNotFound;
+    }
+
+    public void lagre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
